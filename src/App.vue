64cd878709f45d4f4bda1108acb7f4ac059db4b6
Radosław Śliwa <radoslaw.sliwa@execon.pl>
2021-06-13 20:46:52 +0200

    basic refactor

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <component :is="layout">
         <div id="calculator">
             <Toggle />
-            <Screen :number="currValue" />
+            <Screen :number="currScreenValue" />
             <Controller @key-value="interceptor" />
         </div>
     </component>
@@ -26,16 +26,71 @@ export default {
         Controller,
     },
     setup() {
-        const accumulator = reactive([]);
-        let currValue = ref(null);
-        const interceptor = (input) => {
-            accumulator.push(input);
-            currValue.value = accumulator.join('');
+        let allValuesArr = reactive([]);
+        let accumulatedValue = ref(null);
+        let currScreenValue = ref('');
+        let operator = ref(null);
+        let wasOperatorSelected = ref(operator.value);
+
+        function reset() {
+            allValuesArr = reactive([]);
+            accumulatedValue.value = null;
+            currScreenValue.value = '';
+            operator.value = null;
+            wasOperatorSelected.value = operator.value;
+        }
+
+        const interceptor = (btnValue) => {
+            const input = ref(btnValue);
+            const inputValue = input.value.altValue ?? input.value.value;
+            if (input.value.type === 'operator') {
+                operatorHandler(input.value);
+                wasOperatorSelected.value = true;
+                return;
+            }
+            if (input.value.type === 'function') {
+                functionHandler(input.value);
+                return;
+            }
+            if (wasOperatorSelected.value && input.value.type === 'number') {
+                // eslint-disable-next-line no-eval
+                accumulatedValue.value = eval(`${accumulatedValue.value} ${operator.value} ${Number(inputValue)}`);
+                // eslint-disable-next-line no-eval
+                currScreenValue.value = accumulatedValue.value;
+                allValuesArr = [];
+                operatorHandler(input.value, true);
+            }
+            if ((input.value.type === 'number' || input.value.name === 'dot') && !wasOperatorSelected.value) {
+                allValuesArr.push(inputValue);
+                currScreenValue.value = allValuesArr.join('');
+            }
+            console.log('operator value ', operator.value);
+            console.log('accumulatedVal ', accumulatedValue.value);
+            console.log('allValuesArr ', allValuesArr);
+            console.log('currScreenValue ', currScreenValue.value);
         };
-        console.log('CURR VALUE ', currValue);
+        const functionHandler = (currFunction) => {
+            switch (currFunction.name) {
+            case 'del':
+                allValuesArr.pop();
+                currScreenValue.value = allValuesArr.join('');
+                break;
+            case 'reset':
+                reset();
+                break;
+            default:
+                currScreenValue.value = null;
+            }
+        };
+        const operatorHandler = (currOperator) => {
+            console.log('CURR OPERATOR ', currOperator);
+            accumulatedValue.value = accumulatedValue.value ?? Number(currScreenValue.value);
+            currScreenValue.value = accumulatedValue.value;
+            operator.value = currOperator.value;
+        };
         return {
             interceptor,
-            currValue,
+            currScreenValue,
         };
     },
     data() {
@@ -47,20 +102,21 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/styles/main.scss';
-#app {
-    font-family: 'Spartan';
-    background: $mainBackground;
-    position: relative;
-    min-height: 100vh;
-    min-width: 100vw;
-    #calculator {
-        position: absolute;
-        height: auto;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        border-radius: $mainRadius;
+    @import './assets/styles/main.scss';
+    #app {
+        font-family: 'Spartan';
+        background: $mainBackground;
+        position: relative;
+        min-height: 100vh;
+        min-width: 100vw;
+        #calculator {
+            max-height: 85vh;
+            position: absolute;
+            height: auto;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            border-radius: $mainRadius;
+        }
     }
-}
 </style>
--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="controller">
-        <div v-for="(el, index) in config"
-             :key="el + index"
-             tabindex="0"
-             role="button"
-             :class="[`button button-${el.name}`, {'active':el.value===currentValue}]"
-             @click="clickHandler(el)"
+        <div
+            v-for="(el, index) in config"
+            :key="el + index"
+            tabindex="0"
+            role="button"
+            :class="[`button button-${el.name}`, { active: el.value === currentValue }]"
+            @click="clickHandler(el)"
         >
             {{ el.value }}
         </div>
@@ -14,20 +15,20 @@
 
 <script>
 import { ref } from 'vue';
-import { config } from './Buttons/buttonsConfig';
+import { config } from '../buttonsConfig';
 
 export default {
     name: 'Controller',
     setup(props, { emit }) {
         let currentValue = ref(null);
         const clickHandler = (val) => {
-            currentValue.value = val.value;
+            currentValue.value = val;
             if (val.name === 'reset') {
                 setTimeout(() => {
                     currentValue.value = null;
                 }, 300);
             }
-            emit('key-value', currentValue.value);
+            emit('key-value', currentValue);
         };
         return {
             clickHandler,
@@ -44,15 +45,15 @@ export default {
 </script>
 
 <style lang="scss">
-.controller {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr;
-    padding: 1.25rem;
-    border-radius: $mainRadius;
-    column-gap: 1rem;
-    row-gap: 1rem;
-    background-color: $deviceBackground;
-    margin-top: 1.5rem;
-}
+    .controller {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
+        padding: 1.25rem;
+        border-radius: $mainRadius;
+        column-gap: 1rem;
+        row-gap: 1rem;
+        background-color: $deviceBackground;
+        margin-top: 1.5rem;
+    }
 </style>
