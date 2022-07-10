3606d9ecaf9420f516f4e8cbbfabccd5d8fdbd0c
Radosław Śliwa <r.sliwa@digitalcolliers.com>
2022-07-10 15:19:24 +0200

    moved to vite

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable quotes */
+// vite.config.ts
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '/src'),
+        },
+    },
+    define: {
+        'process.env': {},
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/assets/styles/utils/_vars.scss";`
+            }
+        }
+    }
+});
