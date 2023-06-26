15cfb14928960eff106722eae2b888bfca14d3fd
Radek <sliwa.radoslaw2@gmail.com>
2023-06-26 22:23:25 +0200

    floating point in progress

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,9 @@
             <Toggle />
             <Screen :number="currScreenValue" />
             <Controller @key-selected="selectionHandler" />
-            <h2>ACC VALUE {{ accumulatedValue }}</h2>
             <h2>OPERATOR {{ operator }}</h2>
-            <h2>ACTIVE SIDE {{ activeSide }}</h2>
-            <h2>LEFT SIDE {{ leftSideValue }}</h2>
-            <h2>RIGHT SIDE {{ rightSideValue }}</h2>
+            <h2>ALL VALUES ARR {{ allValuesArr }}</h2>
+            <h2>ACCUMULATED VALUE {{ accumulatedValue }}</h2>
         </div>
     </Layout>
 </template>
@@ -21,14 +19,10 @@ import Toggle from './components/Toggle.vue'
 import Screen from './components/Screen.vue'
 import { IButton, TValue, TButtonValue } from './types'
 
-let leftSideValue = ''
-let rightSideValue = ''
-
-const activeSide = ref<'left' | 'right'>('left')
-
-let accumulatedValue: string | number | null = ''
 const currScreenValue = ref<string | number>(0)
 const operator = ref<null | TButtonValue>()
+const allValuesArr = ref<Array<string | number>>([])
+const accumulatedValue = ref<number>(0)
 
 const selectionHandler = (button: IButton) => {
     const { value, type, name } = button
@@ -49,31 +43,29 @@ const selectionHandler = (button: IButton) => {
     }
 }
 
-const numberHandler = (number: TButtonValue) => {
-    if (activeSide.value === 'left' && !accumulatedValue) leftSideValue += number
-    else rightSideValue += number
-    console.log('leftSideValue, rightSideValue: ', leftSideValue, rightSideValue)
+const numberHandler = (clickedValue: TButtonValue) => {
+    const isPoint = clickedValue === '.'
 
-    console.log('accumulatedValue in NUMBER HANDLER: ', accumulatedValue)
+    allValuesArr.value.push(clickedValue)
 
-    currScreenValue.value = activeSide.value === 'left' ? leftSideValue : rightSideValue
+    if (operator.value) {
+        accumulatedValue.value = eval(allValuesArr.value.join(''))
+        currScreenValue.value = !isPoint ? clickedValue : currScreenValue.value + '.'
+        // operator.value = null
+        return
+    }
+    currScreenValue.value = allValuesArr.value.join('')
 }
 
 const operatorHandler = (currOperator: TButtonValue) => {
-    if (!rightSideValue && leftSideValue) {
-        activeSide.value = activeSide.value === 'left' ? 'right' : 'left'
-        operator.value = currOperator
-        return
+    if (operator.value) {
+        console.log('yooo', eval(allValuesArr.value.join('')))
+        accumulatedValue.value = eval(allValuesArr.value.join(''))
+
+        currScreenValue.value = accumulatedValue.value
+        // return
     }
-    if (leftSideValue && rightSideValue) {
-        accumulatedValue = eval(`${leftSideValue} + ${accumulatedValue} ${operator.value} ${rightSideValue}`)
-        console.log('ACCUMULATED VALUE, right', Number(accumulatedValue), Number(rightSideValue))
-        // debugger
-        currScreenValue.value = accumulatedValue
-        leftSideValue = ''
-        rightSideValue = ''
-    }
-    console.log('OPERATOR, accumulatedValue, activeSide.value, leftSideValue', accumulatedValue, activeSide.value, leftSideValue)
+    allValuesArr.value.push(currOperator)
     operator.value = currOperator
 }
 
